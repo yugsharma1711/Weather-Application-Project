5cee24b487995eec14cac73198b51af5a77bddeb
Yug Sharma <imperialyug0805@gmail.com>
2023-05-11 11:50:05 +0530

    Added time functionality || error -m Time not correct

--- frontend/frontend/src/components/Weather.jsx
+++ frontend/frontend/src/components/Weather.jsx
@@ -1,17 +1,55 @@
 import axios from 'axios'
 import { useState } from 'react';
+import { useEffect } from 'react';
 const Weather = ()=>{
     const [data, setData] = useState({});
     const [location, setLocation] = useState('');
+    const [date, setDate] = useState();
+    var d = new Date((new Date().getTime()))
+    d = d.toISOString()
     const searchLocation = async (event) => {
         console.log(event.target.value);
         if (event.key === 'Enter') {
           await axios.get(`http://127.0.0.1:8000/api/fetchWeather/${event.target.value}/`).then((response) => {
             setData(response.data);
             console.log(response.data);
-          });
+            // console.log(typeof response.data.timezone)
+            const timezoneOffset = response.data.timezone * 1000;
+            const updatedDate = new Date(Date.now() + timezoneOffset).toISOString();
+            setDate(updatedDate);
+        });
+        
           setLocation('');
         }
+      };
+      useEffect(() => {
+        const interval = setInterval(() => {
+          const currentTime = new Date();
+          const updatedDate = new Date(currentTime.getTime() + (data.timezone * 1000)).toISOString();
+          setDate(updatedDate);
+        }, 1000);
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, [data]);
+      const formatTime = (timeString) => {
+        const options = {
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          hour12 : 'false'
+        };
+        return new Date(timeString).toLocaleTimeString('en-us');
+      };
+      const formatDate = (timeString) => {
+        const options = {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          weekday: 'long',
+        };
+        return new Date(timeString).toLocaleDateString(undefined, options);
       };
     return (
         <>
@@ -27,6 +65,15 @@ const Weather = ()=>{
                 <div className="top">
                     <div className="location">
                         <p>{data.name} {data.sys ? <p style = {{'font-size' : '20px'}}>{data.sys.country}</p> : null}</p>
+                        {data.sys && (
+                            <div>
+                                Current Date: {formatDate(date)}
+                                <br />
+                                Current Time: {formatTime(date)}
+                                
+                            </div>
+                            )
+                        }
                     </div>
                     <div className="temp">
                         {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
