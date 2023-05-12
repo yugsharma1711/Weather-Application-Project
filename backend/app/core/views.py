1a0da77a95d794736d6a2894b47e8d1e4fd1e9c4
Yug Sharma <imperialyug0805@gmail.com>
2023-05-12 17:13:13 +0530

    Merged DEV -- Releasing version 2.0

--- backend/app/core/views.py
+++ backend/app/core/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 class get_weather(APIView):
  def get(self, request, location):
         print(location)
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&units=imperial&appid=e24140ce16353431563ed70907d29f4a'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid=e24140ce16353431563ed70907d29f4a'
         response = requests.get(url)
         data = response.json()
         return Response(data)
